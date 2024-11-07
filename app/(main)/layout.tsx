439c747f98c0ef479f2ed803f71612420dd6a3d3
CarsonH19 <carsonharp.fullstack@gmail.com>
2024-11-06 21:31:04 -0500

    feat: clerk icon

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
 type Props = {
   children: React.ReactNode;
 };
@@ -5,7 +8,17 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <main className="h-full bg-background">{children}</main>
+      <main className="h-full bg-background">
+        <div className=" absolute top-0 right-0 p-4">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+        </div>
+        {children}
+      </main>
     </>
   );
 };
