1125097ed16bde37f88f199f85d6545bc4d85df4
CarsonH19 <carsonharp.fullstack@gmail.com>
2024-11-08 17:16:23 -0500

    fix: button styling

--- components/modals/tomes/tome-answers.tsx
+++ components/modals/tomes/tome-answers.tsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 
+import { Button } from "@/components/ui/button";
+
 export default function TomeAnswers({
   answers,
   selectedAnswer,
@@ -20,30 +22,28 @@ export default function TomeAnswers({
         let cssClass = "";
 
         if (answerState === "answered" && isSelected) {
-          cssClass = "bg-orange-300"; 
+          cssClass = "bg-orange-300";
         }
 
         if (
           (answerState === "correct" || answerState === "wrong") &&
           isSelected
         ) {
-          cssClass =
-            answerState === "correct"
-              ? "bg-green-400"
-              : "bg-red-400";
+          cssClass = answerState === "correct" ? "bg-green-400" : "bg-red-400";
         }
 
         return (
           <li key={answer} className="mb-4">
-            <button
+            <Button
+              variant={"secondary"}
               onClick={() => onSelect(answer)}
-              className={`w-full p-4 rounded-md ${
-                cssClass || "bg-gray-100 hover:bg-orange-300"
+              className={`w-full h-auto p-4 rounded-md whitespace-normal break-words ${
+                cssClass || "hover:bg-orange-300"
               }`}
               disabled={answerState !== ""}
             >
               {answer}
-            </button>
+            </Button>
           </li>
         );
       })}
